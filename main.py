9174c51e7bdcb04674f39f458e292498bed96714
Jimmy Alexander <>
2022-09-13 16:34:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+
+print ("Bienvenido a mi trivia sobre basquet-NBA")
+print ("Pondremos a prueba tus conocimientos")
+
+print ("Responder las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Entrer' para enviar tu respuesta:\n")
+
+print ("1) ¿Quién tiene mas triples anotados en la NBA?")
+print ("a) Lebron James")
+print ("b) Michael Jordan")
+print ("c) Stephen Curry")
+print ("d) Klay Thompson")
+
+
+respuesta_1 = input("\nTu respuesta: ")
+if respuesta_1 == "c":
+  print("Muy bien!")
+else:
+  print("incorrecto")
+
+print ("\n2) ¿Quién tiene mas puntos anotados en la historia de la NBA?")
+print ("a) Michael Jordan")
+print ("b) Kobe Bryan")
+print ("c) Lebron James")
+print ("d) Kareem Abdul-Jabbar")
+
+
+respuesta_2 = input("\nTu respuesta: ")
+if respuesta_2 == "d":
+  print("Excelente!")
+else:
+  print("incorrecto")
+
+
+print ("\n3) ¿Que jugador tiene mas anillos obtenidos en la historia de la NBA?")
+print ("a) Michael Jordan")
+print ("b) Stephen Curry")
+print ("c) Larry Bird")
+print ("d) Bill Russell")
+
+
+respuesta_3 = input("\nTu respuesta: ")
+if respuesta_3 == "d":
+  print("Grandioso!")
+else:
+  print("incorrecto")
+
+
+print("\n MUCHAS GRACIAS POR TU PARTICIPACION")
